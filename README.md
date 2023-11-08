f95136e4b61916e3b8aa46984130ad69979b3257
theportals <clitchedglock94@gmail.com>
2023-11-07 18:40:44 -0700

    Optimize imports; add README; update .gitignore

---
+++ README.md
@@ -0,0 +1,2 @@
+# Genre Reveal
+For CS5030 Fall 2023 final project. This project uses the k-means clustering algorithm to cluster 1.2M songs using metrics provided by Spotify, using serial, shared-memory parallel CPU, shared-memory parallel GPU, distributed-memory CPU, and distributed-memory GPU implementations.
--- visualize.py
+++ visualize.py
@@ -1,15 +1,9 @@
-import matplotlib.colors
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
 import pandas as pd
-import seaborn as sns
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 df = pd.read_csv("output.csv")
-# sns.scatterplot(x=df.x, y=df.y, z=df.z, hue=df.c, palette=sns.color_palette("hls", n_colors=10))
 ax.scatter(df.x, df.y, df.z, c=df.c)
 ax.set_xlabel("X axis")
 ax.set_ylabel("Y axis")
